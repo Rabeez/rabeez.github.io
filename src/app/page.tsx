2621e59bf6838d91743508470c043ce7774ed1f8
Rabeez Riaz <ruoter123@gmail.com>
2025-04-26 00:11:09 +0400

    Hover animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 bg-ctp-base p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <div className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-lg">
+        <div className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-lg motion-duration-3000 hover:motion-text-out-ctp-teal-500">
           Hello @, World!
         </div>
-        <div className="motion-blur-in-md motion-opacity-in-0 motion-translate-y-in-100 text-ctp-mauve-700">
+        <div className="motion-blur-in-md motion-opacity-in-0 motion-translate-y-in-100 text-ctp-mauve-700 motion-duration-3000">
           a@nother <span className="italic">one@</span>
         </div>
       </main>
