7eb953ec21fe9854465c0e4573d317b700be982c
Rabeez Riaz <ruoter123@gmail.com>
2025-04-26 14:49:27 +0400

    Refactor modal registry

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,7 @@ export default function Home() {
         id="content"
         className="grid h-screen min-h-full w-screen min-w-full grid-cols-5 gap-0"
       >
-        <div className="col-span-2 block h-full w-full bg-red-300">
-          {" "}
-          asdasf{" "}
-        </div>
+        <div className="col-span-2 block h-full w-full bg-red-300">asdasf</div>
         <div className="col-span-3 block h-full w-full bg-green-300">
           {" "}
           {Object.entries(modals).map(([modalId, { Button, Modal }]) => (
