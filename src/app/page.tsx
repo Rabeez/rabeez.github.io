94193852be4b9f2c62ef1f95b2d727fafac62eae
Rabeez Riaz <ruoter123@gmail.com>
2025-04-26 15:24:34 +0400

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
       >
         <div className="col-span-2 block h-full w-full bg-red-300">asdasf</div>
         <div className="col-span-3 block h-full w-full bg-green-300">
-          {" "}
           {Object.entries(modals).map(
             ([modalId, { Button, Modal, Header }]) => (
               <ModalButton
