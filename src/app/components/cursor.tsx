3efbc9e91cc2706a96269f863184390d28188cfa
Rabeez Riaz <ruoter123@gmail.com>
2025-05-07 03:23:22 +0400

    Improve cursor sizing

--- src/app/components/cursor.tsx
+++ src/app/components/cursor.tsx
@@ -8,10 +8,17 @@ export default function CustomCursor() {
   const y = useMotionValue(0);
   const [isHovering, setIsHovering] = useState(false);
 
+  const make_radius = function (isHovering: boolean) {
+    return isHovering ? 12 : 8;
+  };
+  const make_offset = function (isHovering: boolean) {
+    return isHovering ? 15 : 2;
+  };
+
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
-      x.set(e.clientX - 12);
-      y.set(e.clientY - 12);
+      x.set(e.clientX - make_radius(isHovering) - make_offset(isHovering));
+      y.set(e.clientY - make_radius(isHovering) - make_offset(isHovering));
     };
 
     const handleMouseOver = (e: MouseEvent) => {
@@ -51,15 +58,15 @@ export default function CustomCursor() {
 
   return (
     <motion.div
-      className="pointer-events-none fixed top-0 left-0 z-[9999] opacity-70"
+      className="pointer-events-none fixed top-0 left-0 z-[9999] border-1 border-neutral-500 opacity-70"
       style={{
         x,
         y,
         borderRadius: "9999px",
       }}
       animate={{
-        width: isHovering ? 24 : 16,
-        height: isHovering ? 24 : 16,
+        width: make_radius(isHovering) * 2,
+        height: make_radius(isHovering) * 2,
         backgroundColor: isHovering
           ? "var(--color-primary)"
           : "var(--color-neutral)",
